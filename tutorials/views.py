57691937cc375334dd02756fd2aeef20b8e2b6c9
Fantamady Sylla <32784297+fms294@users.noreply.github.com>
2021-11-11 21:17:10 -0500

    Update views.py

--- tutorials/views.py
+++ tutorials/views.py
@@ -52,7 +52,7 @@ def tutorial_list(request):
         return JsonResponse(tutorials_serializer.data, safe=False)
         # 'safe=False' for objects serialization
 
-    elif request.method == 'POST':
+     elif request.method == 'POST':
         tutorial_data = JSONParser().parse(request)
         tutorial_serializer = TutorialSerializer(data=tutorial_data)
         if tutorial_serializer.is_valid():
